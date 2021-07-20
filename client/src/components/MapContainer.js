2d7e2c2d0c96f48dd8fe1c3d92a45cfa59418367
Lee <joejager@3c22fb02c727.ant.amazon.com>
2021-07-20 16:57:34 -0700

    map functionality still being worked on

---
+++ client/src/components/MapContainer.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+​
+const MapContainer = () => {
+  
+    const locations = [
+        {
+          name: "Location 1",
+          location: { 
+            lat: 41.3954,
+            lng: 2.162 
+          },
+        },
+        {
+          name: "Location 2",
+          location: { 
+            lat: 41.3917,
+            lng: 2.1649
+          },
+        },
+        {
+          name: "Location 3",
+          location: { 
+            lat: 41.3773,
+            lng: 2.1585
+          },
+        },
+        {
+          name: "Location 4",
+          location: { 
+            lat: 41.3797,
+            lng: 2.1682
+          },
+        },
+        {
+          name: "Location 5",
+          location: { 
+            lat: 41.4055,
+            lng: 2.1915
+          },
+        }
+      ];
+      
+
+  const mapStyles = {        
+    height: "100vh",
+    width: "100%"};
+  
+  const defaultCenter = {
+    lat: 41.3851, lng: 2.1734
+  }
+  
+  return (
+     <LoadScript
+       googleMapsApiKey='AIzaSyB54lD_3Rfa6gMLVnzVzbxUI-zZfLWCDr8'>
+        <GoogleMap
+          mapContainerStyle={mapStyles}
+          zoom={13}
+          center={defaultCenter}>
+          {
+            locations.map(item => {
+              return (
+              <Marker key={item.name} 
+                position={item.location}
+                onClick={() => onSelect(item)}
+              />
+              )
+            })
+         }
+        {
+            selected.location && 
+            (
+              <InfoWindow
+              position={selected.location}
+              clickable={true}
+              onCloseClick={() => setSelected({})}
+            >
+              <p>{selected.name}</p>
+            </InfoWindow>
+            )
+         }
+     </GoogleMap>
+     </LoadScript>
+  )
+}
+​
+export default MapContainer;
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import Header from '../components/Header'
+import Login from '../components/Login'
+import Map from '../components/MapContainer'
+
+function Landing() {
+   
+    return (
+    
+        <div>
+            <Header />
+   
+            <Login />
+
+
+        </div>
+    )
+}
+
+export default Landing
