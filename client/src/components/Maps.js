6b4f52e16e785b529157c50e1e63b5115d35002a
Lee <joejager@3c22fb02c727.ant.amazon.com>
2021-07-21 13:13:15 -0700

    made to merge

--- client/src/components/Maps.js
+++ client/src/components/Maps.js
@@ -1,9 +1,21 @@
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 import React from "react";
 import googleMapStyles from "../assets/GoogleMapsStyle";
+import { Button, Popover, PopoverHeader, PopoverBody } from 'reactstrap';
 
 export class Maps extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      latArr: [47.6740,47.9790],
+      lngArr: [-122.1215,-122.2021]
+    };
+  }
+
   render() {
+    let latArr = [47.6740,47.9790];
+    let lngArr = [-122.1215,-122.2021];
     return this.props[0].map((index, val) => (
       <div
         style={{
@@ -19,9 +31,17 @@ export class Maps extends React.Component {
           google={this.props.google}
           zoom={15}
           styles={index.Style}
-          initialCenter={{ lat: 9.761927, lng: 79.95244 }}
+          initialCenter={{ lat: 47.6062, lng: -122.3321 }}
         >
-          <Marker position={{ lat: 9.761927, lng: 79.95244 }} />
+          
+          {latArr.map((item,index)=>{
+            console.log(this.state.lngArr)
+            return <Marker position={{lat: this.state.latArr[index],lng: this.state.lngArr[index]}} />
+          })
+
+          }
+        
+        
         </Map>
       </div>
     ));
