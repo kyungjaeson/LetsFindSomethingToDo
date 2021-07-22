f78992bcbaff21414e17aeda7ea8ff46e4873d90
Kerri-Leigh Grady <klgrady@gmail.com>
2021-07-22 16:18:06 -0400

    Styled the popup event window and added get directions, links, and date formatting

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -4,6 +4,8 @@ import NewWindow from 'react-new-window';
 function Event(props) {
     const [event, setEvent] = useState([]);
     const id = props.id;
+    const lat = props.latitude;
+    const lng = props.longitude;
 
     useEffect( () => {
         var url = '/eventDetail?id=' + id;
@@ -24,20 +26,40 @@ function Event(props) {
             .catch(err => console.log(err))
 
     }, [id]);
+
+    const formatDate = (date) => {
+        const options = { year: "numeric", month: "long", day: "numeric" };
+        return new Date(date).toLocaleDateString(undefined, options);
+    }
+
+    const getAddress = (loc) => {
+        if (loc === undefined) {
+            return "";
+        }
+
+        return loc.display_address;
+    }
     
+    const getDirectionsUrl = () => {
+        return "https://www.google.com/maps/dir/?api=1&destination=" + lat + ',' + lng;
+    }
+
+
     return (
-            <NewWindow>
-                <div id="name">{event.name}</div>
+        <NewWindow>
+            <div class="container">
+                <div class="row"><div id="event-name">{event.name}</div></div>
                 <div id="id">{event.id}</div>
-                <div id="url">{event.event_site_url}</div>
-                <div id="ticketurl">{event.tickets_url}</div>
-                <div id="date">{event.time_start}</div>
-                <div id="dateend">{event.time_end}</div>
-                <div id="img"><img alt={event.name} src={event.image_url} /></div>
-                <div id="description">{event.description}</div>
-                <div id="cost">{event.cost}</div>
-                <div id="location lat">{event.latitude}</div>
-                <div id="location long">{event.longitude}</div>
+                <div class="row">
+                    <div class="col" id="event-cost">Event Cost: ${event.cost}</div>
+                    <div class="col" id="event-url"><a target="_blank" href={event.event_site_url}>Event Site</a></div>
+                    <div class="col" id="event-ticketurl"><a target="_blank" href={event.tickets_url}>Purchase Tickets</a></div>
+                </div>
+                <div class="row" id="event-img"><img id="event" alt={event.name} src={event.image_url} /></div>
+                <div class="row" id="event-date">{formatDate(event.time_start)}</div>
+                <div class="row" id="event-description">{event.description}</div>
+                <div id="event-address"><a target="_blank" href={getDirectionsUrl()}>Get Directions</a> to {getAddress(event.location)}</div>
+            </div>
         </NewWindow>
     )
 }
